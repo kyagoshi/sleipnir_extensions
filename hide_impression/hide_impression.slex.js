913a77fda66876d9104c91a5a702c4a0dc231aba
keisuke.miyazaki <zakimiya.k0425@gmail.com>
2026-01-18 22:25:03 +0900

    Remove deprecated GitHub Actions workflow and update hide_impression script metadata

--- hide_impression/hide_impression.slex.js
+++ hide_impression/hide_impression.slex.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name        Hide Impression
-// @name:ja     感想非表示
-// @author      keisuke-miyazaki
+// @name        Hide Impressions on Syosetukaninarou
+// @name:ja     小説家になろう感想非表示
+// @author      kyagoshi
 // @description Hides impression elements on Syosetu sites.
 // @description:ja 小説家になろう・ノクターンノベルズの感想欄を非表示にします。
 // @include     https://ncode.syosetu.com/*/*/
